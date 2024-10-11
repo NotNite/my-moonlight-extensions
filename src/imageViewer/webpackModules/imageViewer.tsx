300459e680dacf5463ba65fb516d030f973a6a18
Dissssy <ethanconaway@yahoo.com>
2024-10-11 19:33:17 -0400

    i'd like to put in my letter of resignation now

--- src/imageViewer/webpackModules/imageViewer.tsx
+++ src/imageViewer/webpackModules/imageViewer.tsx
@@ -89,8 +89,8 @@ export default function ImageViewer({
     (e: MouseEvent) => {
       if (!dragging) return;
 
-      setX((prevX) => prevX + e.movementX / zoom);
-      setY((prevY) => prevY + e.movementY / zoom);
+      setX((prevX) => prevX + e.movementX / (zoom * 2.0));
+      setY((prevY) => prevY + e.movementY / (zoom * 2.0));
     },
     [dragging, zoom]
   );
@@ -117,7 +117,7 @@ export default function ImageViewer({
       const newZoom = zoom + (-deltaY / 100) * zoom;
       const newZoomClamped = Math.min(
         20,
-        Math.max(calculatedScale / 10, newZoom)
+        Math.max(0.01, newZoom)
       );
       setZoom(newZoomClamped);
     },
@@ -244,7 +244,7 @@ export default function ImageViewer({
           tooltipPosition="top"
           icon={PlusLargeIcon}
           onClick={() => {
-            setZoom((prevZoom) => prevZoom + 0.1);
+            setZoom((prevZoom) => Math.min(20.0, prevZoom + 0.1));
           }}
         />
         <HeaderBar.Icon
@@ -252,7 +252,7 @@ export default function ImageViewer({
           tooltipPosition="top"
           icon={MinusIcon}
           onClick={() => {
-            setZoom((prevZoom) => prevZoom - 0.1);
+            setZoom((prevZoom) => Math.max(0.01, prevZoom - 0.1));
           }}
         />
 

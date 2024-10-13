9a29f3ff8316628f3985259e51d27ab917f057bf
NotNite <hi@notnite.com>
2024-10-13 13:04:12 -0400

    Refactor media fetcher Windows stuff

--- src/mediaControls/media-fetcher/src/windows.rs
+++ src/mediaControls/media-fetcher/src/windows.rs
@@ -4,7 +4,8 @@ use crate::{
 };
 use async_trait::async_trait;
 use base64::Engine;
-use std::io::Cursor;
+use std::{io::Cursor, sync::Arc};
+use tokio::sync::Mutex;
 use windows::{
     Media::{
         Control::{
@@ -21,105 +22,99 @@ const WHAT_ARE_THEY_DOING_AT_MICROSOFT: f64 = 10_000_000.;
 
 #[derive(Default)]
 pub struct WindowsMediaFetcher {
-    session_manager: Option<GlobalSystemMediaTransportControlsSessionManager>,
+    session_manager: Option<Arc<Mutex<GlobalSystemMediaTransportControlsSessionManager>>>,
 }
 
 impl WindowsMediaFetcher {
-    async fn run_until_session_closes(
+    async fn get_status(
         &self,
         session: GlobalSystemMediaTransportControlsSession,
-    ) -> anyhow::Result<()> {
-        let mut status = PlaybackStatus::default();
+    ) -> anyhow::Result<PlaybackStatus> {
+        let timeline_properties = session.GetTimelineProperties()?;
+        let playback_info = session.GetPlaybackInfo()?;
+        let playback_status = playback_info.PlaybackStatus()?;
 
-        loop {
-            let timeline_properties = session.GetTimelineProperties()?;
-            let playback_info = session.GetPlaybackInfo()?;
-            let playback_status = playback_info.PlaybackStatus()?;
-            if playback_status == GlobalSystemMediaTransportControlsSessionPlaybackStatus::Closed {
-                // This never triggered for me but just in case
-                return Ok(());
-            }
+        let elapsed = timeline_properties.Position().unwrap_or_default().Duration as f64
+            / WHAT_ARE_THEY_DOING_AT_MICROSOFT;
+        let duration = timeline_properties.EndTime().unwrap_or_default().Duration as f64
+            / WHAT_ARE_THEY_DOING_AT_MICROSOFT;
 
-            let elapsed = timeline_properties.Position().unwrap_or_default().Duration as f64
-                / WHAT_ARE_THEY_DOING_AT_MICROSOFT;
-            let duration = timeline_properties.EndTime().unwrap_or_default().Duration as f64
-                / WHAT_ARE_THEY_DOING_AT_MICROSOFT;
+        let mut new_status = PlaybackStatus {
+            title: String::new(),
+            artist: String::new(),
+            elapsed,
+            duration,
+            playing: playback_status
+                == GlobalSystemMediaTransportControlsSessionPlaybackStatus::Playing,
+            repeat: match playback_info
+                .AutoRepeatMode()
+                .ok()
+                .map(|x| x.Value().ok())
+                .flatten()
+                .unwrap_or(MediaPlaybackAutoRepeatMode::None)
+            {
+                MediaPlaybackAutoRepeatMode::List => crate::proto::RepeatMode::All,
+                MediaPlaybackAutoRepeatMode::Track => crate::proto::RepeatMode::One,
+                _ => crate::proto::RepeatMode::None,
+            },
+            shuffle: playback_info
+                .IsShuffleActive()
+                .ok()
+                .map(|x| x.Value().ok())
+                .flatten()
+                .unwrap_or(false),
+        };
 
-            let mut new_status = PlaybackStatus {
-                title: String::new(),
-                artist: String::new(),
-                elapsed,
-                duration,
-                playing: playback_status
-                    == GlobalSystemMediaTransportControlsSessionPlaybackStatus::Playing,
-                repeat: match playback_info
-                    .AutoRepeatMode()
-                    .ok()
-                    .map(|x| x.Value().ok())
-                    .flatten()
-                    .unwrap_or(MediaPlaybackAutoRepeatMode::None)
-                {
-                    MediaPlaybackAutoRepeatMode::List => crate::proto::RepeatMode::All,
-                    MediaPlaybackAutoRepeatMode::Track => crate::proto::RepeatMode::One,
-                    _ => crate::proto::RepeatMode::None,
-                },
-                shuffle: playback_info
-                    .IsShuffleActive()
-                    .ok()
-                    .map(|x| x.Value().ok())
-                    .flatten()
-                    .unwrap_or(false),
-            };
-
-            if let Ok(media_properties) = session.TryGetMediaPropertiesAsync()?.await {
-                new_status.title = media_properties
-                    .Title()
-                    .unwrap_or_default()
-                    .to_string_lossy();
-                new_status.artist = media_properties
-                    .Artist()
-                    .unwrap_or_default()
-                    .to_string_lossy();
-            }
-
-            // Timeline properties don't update immediately
-            if new_status != status {
-                status = new_status.clone();
-                send_response(crate::proto::Response::PlaybackStatus(new_status))?;
-            }
-
-            tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+        if let Ok(media_properties) = session.TryGetMediaPropertiesAsync()?.await {
+            new_status.title = media_properties
+                .Title()
+                .unwrap_or_default()
+                .to_string_lossy();
+            new_status.artist = media_properties
+                .Artist()
+                .unwrap_or_default()
+                .to_string_lossy();
         }
+
+        Ok(new_status)
     }
 }
 
 #[async_trait]
 impl MediaFetcher for WindowsMediaFetcher {
     async fn init(&mut self) -> anyhow::Result<()> {
-        self.session_manager =
-            Some(GlobalSystemMediaTransportControlsSessionManager::RequestAsync()?.await?);
+        let session_manager =
+            GlobalSystemMediaTransportControlsSessionManager::RequestAsync()?.await?;
+        self.session_manager = Some(Arc::new(Mutex::new(session_manager)));
         Ok(())
     }
 
     async fn run(&self) -> anyhow::Result<()> {
         let manager = self.session_manager.as_ref().unwrap();
-        let mut sent_failure = false;
+        let mut prev_session = PlaybackStatus::default();
 
         loop {
-            // GetCurrentSession will return an error of 0 ("The operation completed successfully" lmao)
-            // if there is no session (= no media playing atm)
-            if let Ok(session) = manager.GetCurrentSession() {
-                sent_failure = false;
-                if let Err(e) = self.run_until_session_closes(session).await {
-                    eprintln!("Error in session: {:?}", e);
+            let new_status = {
+                let manager = manager.lock().await;
+                if let Ok(session) = manager.GetCurrentSession() {
+                    let result = self.get_status(session).await;
+                    if let Ok(result) = result {
+                        // Everything is playing
+                        result
+                    } else {
+                        // Something went wrong, let's pretend nothing is playing
+                        // GetCurrentSession likes to return error 0 ("The operation completed successfully" lmao) sometimes
+                        PlaybackStatus::default()
+                    }
+                } else {
+                    // Nothing is playing
+                    PlaybackStatus::default()
                 }
-            }
+            };
 
-            if !sent_failure {
-                send_response(crate::proto::Response::PlaybackStatus(
-                    PlaybackStatus::default(),
-                ))?;
-                sent_failure = true;
+            if prev_session != new_status {
+                send_response(crate::proto::Response::PlaybackStatus(new_status.clone()))?;
+                prev_session = new_status;
             }
 
             // I hate polling but the events from this kinda suck
@@ -128,18 +123,45 @@ impl MediaFetcher for WindowsMediaFetcher {
     }
 
     async fn handle_command(&self, request: Request) -> anyhow::Result<()> {
+        let session = self
+            .session_manager
+            .as_ref()
+            .unwrap()
+            .lock()
+            .await
+            .GetCurrentSession()?;
+
         match request {
             Request::GetAlbumArt => {
-                let session = self.session_manager.as_ref().unwrap().GetCurrentSession()?;
-                let media_properties = session.TryGetMediaPropertiesAsync()?.await?;
+                let mut media_properties = None;
+
+                // Try to get the media properties multiple times since thie API returns cryptic errors
+                // (more of that error 0 stuff)
+                for _ in 0..5 {
+                    if let Ok(request) = session.TryGetMediaPropertiesAsync() {
+                        if let Ok(data) = request.await {
+                            media_properties = Some(data);
+                            break;
+                        }
+                        break;
+                    }
+
+                    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+                }
+
+                if media_properties.is_none() {
+                    return Ok(());
+                }
+                let media_properties = media_properties.unwrap();
+
                 let thumbnail = media_properties.Thumbnail()?;
 
-                // use .get() here to avoid it complaining about future stuff
+                // Use .get() here to avoid it complaining about future stuff
                 let handle = thumbnail.OpenReadAsync()?.get()?;
                 let stream = handle.GetInputStreamAt(0)?;
                 let reader = DataReader::CreateDataReader(&stream)?;
 
-                // this api sucks so bad
+                // This API sucks so bad
                 reader.LoadAsync(handle.Size()? as u32)?.get()?;
                 let mut buf = vec![0u8; handle.Size()? as usize];
                 reader.ReadBytes(&mut buf)?;
@@ -157,8 +179,6 @@ impl MediaFetcher for WindowsMediaFetcher {
             }
 
             Request::Play | Request::Pause | Request::SkipBackward | Request::SkipForward => {
-                let session = self.session_manager.as_ref().unwrap().GetCurrentSession()?;
-
                 match request {
                     Request::Play | Request::Pause => {
                         let status = session.GetPlaybackInfo()?.PlaybackStatus()?;
@@ -186,7 +206,6 @@ impl MediaFetcher for WindowsMediaFetcher {
             }
 
             Request::SetRepeatMode { mode } => {
-                let session = self.session_manager.as_ref().unwrap().GetCurrentSession()?;
                 session
                     .TryChangeAutoRepeatModeAsync(match mode {
                         crate::proto::RepeatMode::None => MediaPlaybackAutoRepeatMode::None,
@@ -198,7 +217,6 @@ impl MediaFetcher for WindowsMediaFetcher {
             }
 
             Request::SetShuffle { shuffle } => {
-                let session = self.session_manager.as_ref().unwrap().GetCurrentSession()?;
                 session.TryChangeShuffleActiveAsync(shuffle)?.await?;
                 Ok(())
             }

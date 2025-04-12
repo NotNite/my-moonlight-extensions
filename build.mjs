1f845aef6a3199cd351c5b413e222c250db2360b
NotNite <hi@notnite.com>
2025-04-12 10:34:27 -0400

    Update everything

--- build.mjs
+++ build.mjs
@@ -2,6 +2,8 @@ import fs from "node:fs";
 import path from "node:path";
 import { watchExt, buildExt } from "@moonlight-mod/esbuild-config";
 
+const esm = [];
+
 const watch = process.argv.includes("--watch");
 const clean = process.argv.includes("--clean");
 
@@ -11,10 +13,12 @@ if (clean) {
   const exts = fs.readdirSync("./src");
 
   for (const ext of exts) {
+    /** @type {import("@moonlight-mod/esbuild-config").ESBuildFactoryOptions} */
     const cfg = {
-      src: path.resolve(path.join("src", ext)),
-      dst: path.resolve(path.join("dist", ext)),
-      ext
+      ext,
+      entry: path.resolve(path.join("src", ext)),
+      output: path.resolve(path.join("dist", ext)),
+      esm: esm.includes(ext)
     };
 
     if (watch) {
